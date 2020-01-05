95ccaef4bf0e35aa643d5736180f496e593e5400
Dorian Pustina <dorian.pustina@chdifoundation.org>
2020-01-05 01:13:48 -0500

    Fix open browser for Mac

--- antsr/Run.antsr.linux.sh
+++ antsr/Run.antsr.linux.sh
@@ -5,22 +5,29 @@ IMAGE=antsr
 USERNAME=dorianps
 
 read -p "Select a session password for rstudio: " MYPASSWORD
+# check password is not empty
+[[ -z $MYPASSWORD ]] && echo 'Empty password not allowed, existing...' && exit 1
 
 # choosefolder
 VOLUMEMOUNT=""
 read -p "Do you want to mount a folder in the container [y/n]" -n 1 -r
 echo    # (optional) move to a new line
-if [[ ! $REPLY =~ ^[Nn]$ ]]
+if [[ ! $REPLY =~ ^[Nn]$ ]] && [[ ! -z $REPLY ]]
 then
     read -p "Enter a folder to mount in the container: " FOLDER
     VOLUMEMOUNT="-v $FOLDER:/home/rstudio/mydata"
 fi
 
+# browser open command for linux
+OPENCMD='xdg-open'
+# browser open command for Mac
+[[ `uname` == "Darwin" ]] && OPENCMD='open'
+
 # runcontainer
 docker container run $VOLUMEMOUNT -e PASSWORD=$MYPASSWORD -p 8787:8787 -d --name my${IMAGE} --rm ${USERNAME}/${IMAGE}:latest \
 && echo "Browser will open shortly. Enter username: 'rstudio' pass: '$MYPASSWORD'" \
 && sleep 5 \
-&& xdg-open http://localhost:8787 \
+&& $OPENCMD http://localhost:8787 \
 && read -p "Press enter to stop the container..." \
 && echo "Stopping container..." \
 && docker stop my${IMAGE}
--- linda/Run.linda.linux.sh
+++ linda/Run.linda.linux.sh
@@ -5,6 +5,9 @@ IMAGE=linda
 USERNAME=dorianps
 
 read -p "Select a session password for rstudio: " MYPASSWORD
+# check password is not empty
+[[ -z $MYPASSWORD ]] && echo 'Empty password not allowed, existing...' && exit 1
+
 
 # choosefolder
 VOLUMEMOUNT=""
@@ -16,11 +19,16 @@ then
     VOLUMEMOUNT="-v $FOLDER:/home/rstudio/mydata"
 fi
 
+# browser open command for linux
+OPENCMD='xdg-open'
+# browser open command for Mac
+[[ `uname` == "Darwin" ]] && OPENCMD='open'
+
 # runcontainer
 docker container run $VOLUMEMOUNT -e PASSWORD=$MYPASSWORD -p 8787:8787 -d --name my${IMAGE} --rm ${USERNAME}/${IMAGE}:latest \
 && echo "Browser will open shortly. Enter username: 'rstudio' pass: '$MYPASSWORD'" \
 && sleep 5 \
-&& xdg-open http://localhost:8787 \
+&& $OPENCMD http://localhost:8787 \
 && read -p "Press enter to stop the container..." \
 && echo "Stopping container..." \
 && docker stop my${IMAGE}
